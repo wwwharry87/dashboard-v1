fb51518ed650fe0827158235cf3b79d52e2a3143
Wesley Melo <wesleymelo@MacBook-Air-de-Wesley.local>
2025-03-05 00:17:23 -0300

    update do frontend  versão 3.5

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -440,10 +440,10 @@ const Dashboard = () => {
             </table>
           </div>
         </div>
-        {/* Gráfico – sem scroll */}
+        {/* Gráfico – sem scroll e com overflow-hidden para ajustar o conteúdo */}
         <div className={`bg-white rounded-xl shadow-lg p-4 flex flex-col ${tableGraphHeight}`}>
           <h3 className="text-lg font-semibold text-gray-700 mb-2">Movimentação Mensal</h3>
-          <div className="flex-1">
+          <div className="flex-1 overflow-hidden">
             <Bar
               data={{
                 labels: Object.keys(data.entradasSaidasPorMes),
