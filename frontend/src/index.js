9826ad154f32d302a68e4f5e62295d9e8aea939d
Wesley Melo <wesleymelo@MacBook-Air-de-Wesley.local>
2025-04-02 14:04:19 -0300

    update frontend carregar pagina principal v16

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-// Desregistra qualquer service worker antigo para evitar cache desatualizado
+// index.js (adicione no início)
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      registration.unregister();
-      console.log('Service Worker desregistrado:', registration);
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(registration => {
+      registration.unregister().then(() => {
+        console.log('Service Worker desregistrado');
+        caches.keys().then(cacheNames => {
+          cacheNames.forEach(cacheName => caches.delete(cacheName));
+        });
+      });
     });
   });
 }
